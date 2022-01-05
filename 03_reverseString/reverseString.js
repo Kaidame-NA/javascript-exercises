7db7405aa0ff18916ece83303c73d2ec50445b30
Edward Wang <ewang43@jhu.edu>
2022-01-04 21:51:45 -0500

    Finish reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function(str) {
-
+	let stringArr = str.split("");
+	let returnString = "";
+	for(let i = stringArr.length-1; i >= 0; --i)
+	{
+		returnString += stringArr[i];
+	}
+	return returnString;
 };
 
 // Do not edit below this line
